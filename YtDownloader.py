0f095c87ee18fbe0ac0ce10bebc81d2fdcb18eab
Michael Kumsa <michael.kumsa@astu.edu.et>
2022-12-17 00:19:56 -0800

    initial commit

---
+++ YtDownloader.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+
+def Download(link):
+    youtubeOject = YouTube(link)
+    youtubeOject = youtubeOject.streams.get_highest_resolution()
+    try:
+        youtubeOject.download()
+    except:
+        print("There has been some error!")
+    print("Download completed successfully!")
+
+link = input("Enter the youtube url to be downloaded. URL: ")
+Download(link)
